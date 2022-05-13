324f1e7dc01c5638f42a277c0d35baca727b8741
qduHeatLow <1442984671@qq.com>
2022-05-13 19:21:13 +0800

    主文件修改

--- deplay.sh
+++ deplay.sh
@@ -1,6 +1,6 @@
 #上传到github
 git init
 git add -A
-git commit -m '简介修改'
+git commit -m '主文件修改'
 
-git push -f git@github.com:qduHeatLow/aolaxing.git master
+git push -f git@github.com:qduHeatLow/aolaxing.git  develop_2205
